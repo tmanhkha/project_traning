302a900f5fce47b087266bed2080bc11fdf1955d
Manh Kha <kha.tm@raksul.com>
2020-09-16 11:07:34 +0700

    turn off turbolinks in datatable view

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :data_memories
   resources :manufacturers
   devise_for :users
-  root 'home#index'
+  root 'products#index'
   resources :users
   resources :models
 end
